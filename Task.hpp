29565f31ef3b529bd681a17eaa12458c735d7bbc
zian <zw142@duke.edu>
2022-02-14 20:30:25 +0000

    keep-alive

--- Task.hpp
+++ Task.hpp
@@ -27,6 +27,7 @@ public:
         printf("-----------Received Request-----------\n");
         std::cout << response.getRawData().data() << std::endl; 
         printf("-----------End of Request-------------\n");
+        in.sendResponse(response);
         keep_alive();
 	}
 
@@ -34,7 +35,8 @@ public:
         if (request.getField("CONNECTION") != "keep-alive" && request.getField("PROXY-CONNECTION") != "keep-alive")
             return;
         request = std::move(in.recvRequest());
-        HttpResponse response = request.handle(out);
+        out.sendRequest(request);
+        HttpResponse response = out.recvResponse();
         in.sendResponse(response);
         // TODO
         // if request or response is empty, return
