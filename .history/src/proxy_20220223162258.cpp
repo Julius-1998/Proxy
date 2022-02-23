7cef3127663e88710ade91877dd3962c19c1b59d
Jack Zhou <sz232@duke.edu>
2022-02-23 17:51:33 -0500

    daemon

--- .history/src/proxy_20220223162258.cpp
+++ .history/src/proxy_20220223162258.cpp
@@ -14,60 +14,14 @@ SocketBuilder socket_buidler("12345");
 BlockingQueue<Socket> socket_queue(64);
 int cnt;
 int main(int argc, char const *argv[]) {
-    pid_t pid, sid;
-
-	/* Fork off the parent process */
-	pid = fork();
-	if (pid < 0)
-	{
-		exit(EXIT_FAILURE);
-	}
-	/* If we got a good PID, then
-	   we can exit the parent process. */
-	if (pid > 0)
-	{
-		exit(EXIT_SUCCESS);
-	}
-
-	/* Change the file mode mask */
-	umask(0);
-
-	/* Open any logs here */
-
-	/* Create a new SID for the child process */
-	sid = setsid();
-	if (sid < 0)
-	{
-		/* Log the failure */
-		exit(EXIT_FAILURE);
-	}
-
-	/* Change the current working directory */
-	if ((chdir("/")) < 0)
-	{
-		/* Log the failure */
-		exit(EXIT_FAILURE);
-	}
-
-	/* Close out the standard file descriptors */
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-
-	/* Daemon-specific initialization goes here */
-	Signal(SIGPIPE, handler);
+    Signal(SIGPIPE, handler);
 	ThreadPool thread_pool(32);
 	thread_pool.execute(thread_func);
-	int id_counter = 1;
-	/* The Big Loop */
-	while (1)
-	{
-		/* Do some task here ... */
-		socket_queue.push(std::move(socket_buidler.acceptTCPConnection(id_counter++))); // NOTE: move can be omited
-
-		// sleep(30); /* wait 30 seconds */
+    int id_counter = 1;
+	while (1) {
+        socket_queue.push(std::move(socket_buidler.acceptTCPConnection(id_counter++)));  // NOTE: move can be omited
 	}
-	exit(EXIT_SUCCESS);
+	return 0;
 }
 
 
